4fccd3a5728b4f094e58168c7c4240e706b19424
Mark Dyer <heymarky@gmail.com>
2022-09-05 20:31:33 -0700

    Beginnings of "title" of graph and general layout, config

--- applehealthtool/TimeDataGraph.py
+++ applehealthtool/TimeDataGraph.py
@@ -1,12 +1,20 @@
 from typing import Union, Dict
 
 import pygame
+from pygame import SRCALPHA
 from pygame_gui.core import IContainerLikeInterface, UIElement, ObjectID
 from pygame_gui.core.interfaces import IUIManagerInterface
+from pygame_gui.core.utility import premul_alpha_surface
 from pygame_gui.elements import UIImage
 
 
 class UIGraph(UIImage):
+    DEFAULT_CONFIG = {
+        'background_color': pygame.Color(255, 255, 255),
+        'title_font': 'arial',
+        'title_font_size': 20,
+        'title_text_color': pygame.Color(0, 0, 0)
+    }
     def __init__(self,
                  relative_rect: pygame.Rect,
                  manager: IUIManagerInterface,
@@ -16,14 +24,28 @@ class UIGraph(UIImage):
                  anchors: Dict[str, str] = None,
                  visible: int = 1,
                  title='',
+                 title_font_name = None,
+                 title_font_size = 0,
+                 title_text_color = None,
                  data_config=None,
                  data=None
                  ):
+        self.background_color = UIGraph.DEFAULT_CONFIG['background_color']
+
         self._title = title
+        if title_font_name is None:
+            title_font_name = self.DEFAULT_CONFIG['title_font']
+        if title_font_size == 0:
+            title_font_size = self.DEFAULT_CONFIG['title_font_size']
+        if title_text_color is None:
+            self._title_text_color = self.DEFAULT_CONFIG['title_text_color']
+
+        self.set_title_font(title_font_name, title_font_size)
+
         self.set_data(data_config, data)
-        self._surface = pygame.Surface(relative_rect.size)
+        surface = pygame.Surface(relative_rect.size)
         super().__init__(relative_rect,
-                         self._surface,
+                         surface,
                          manager,
                          container=container,
                          parent_element=parent_element,
@@ -31,6 +53,30 @@ class UIGraph(UIImage):
                          anchors=anchors,
                          visible=visible
                          )
+        self.redraw()
+        # IGraph._list_system_fonts()
+
+    @property
+    def background_color(self):
+        return self._background_color
+
+    @background_color.setter
+    def background_color(self, color):
+        self._background_color = color
+
+    @property
+    def title_font(self):
+        return self._title_font
+
+    def set_title_font(self, font_name, font_size):
+        pygame.font.init()
+        print(f'title font is {font_name} / {font_size}')
+        font_name = pygame.font.SysFont(font_name, font_size)
+        self._title_font = font_name
+        # pre render the title
+        self._title_image = self._title_font.render(self._title, True, self._title_text_color) # self.background_color)
+        self._title_image = premul_alpha_surface(self._title_image)
+        print(f'title img: {self._title_image}, color: {self.background_color}')
 
     def set_data(self, config, rows):
         '''
@@ -45,6 +91,24 @@ class UIGraph(UIImage):
         if config is not None:
             assert 'timestamp' in config
             assert 'samples' in config
+
+    def redraw(self):
+        surface = pygame.Surface(self.get_relative_rect().size, flags=SRCALPHA)
+        surface.fill(self._background_color)
+
+        # draw the title
+        if self._title_image:
+            surface.blit(self._title_image, (5,5))
+
+        self.set_image(surface)
+
+    @classmethod
+    def _list_system_fonts(cls):
+        fonts = pygame.font.get_fonts()
+        print(len(fonts))
+        for f in fonts:
+            print(f)
+
 
 class UITimeDataGraph(UIGraph):
     def __init__(self,
